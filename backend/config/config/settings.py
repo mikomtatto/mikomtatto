eec7f4009d7e11ff4152d90274d3513ddc612234
mikomtatto <1y.emre0@gmail.com>
2026-06-05 21:24:59 +0300

    Add Vercel URL to CORS allowed origins

--- backend/config/config/settings.py
+++ backend/config/config/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://mikomtatto.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
